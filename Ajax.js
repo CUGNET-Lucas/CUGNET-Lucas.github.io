cad2d0d0d9c9fe9f686ee2d687303dd494ea6f6d
CUGNET Lucas <118003587+CUGNET-Lucas@users.noreply.github.com>
2023-06-05 08:50:24 +0200

    Add files via upload

--- Ajax.js
+++ Ajax.js
@@ -22,15 +22,8 @@ function loadProjects() {
 function loadResearches() {
     $(document).ready(function(){
         //Charge les éléments p depuis la page "une/url" et les insère dans #re
-        console.log("request for researches");
-        $("#ajaxReceiver").load("VeilleJur.html");
-    });
-}
-function loadTech() {
-    $(document).ready(function(){
-        //Charge les éléments p depuis la page "une/url" et les insère dans #re
-        console.log("request for technology");
-        $("#ajaxReceiver").load("VeilleTech.html");
+        console.log("request for veilles");
+        $("#ajaxReceiver").load("Veille.html");
     });
 }
 function loadCv() {
@@ -39,6 +32,22 @@ function loadCv() {
         console.log("request for cv");
         document.getElementById("ajaxReceiver");
         $("#ajaxReceiver").load("Cv.html");
+    });
+}
+function loadContact() {
+    $(document).ready(function(){
+        //Charge les éléments p depuis la page "une/url" et les insère dans #re
+        console.log("request for contact");
+        document.getElementById("ajaxReceiver");
+        $("#ajaxReceiver").load("Contact.html");
+    });
+}
+function loadLegal() {
+    $(document).ready(function(){
+        //Charge les éléments p depuis la page "une/url" et les insère dans #re
+        console.log("request for contact");
+        document.getElementById("ajaxReceiver");
+        $("#ajaxReceiver").load("Ml.html");
     });
 }
 
@@ -58,5 +67,53 @@ function modalProject(project,command){
 
 function copyright() {
     var theDate=new Date();
-    document.getElementById("footer").innerHTML = '<p><a href="https://www.instagram.com/lucas_cugnet/" target="_blank"><i class="bi bi-instagram" id="insta"></i></a><a href="https://www.facebook.com/people/Lucas-Cugnet/100008483671392/" target="_blank"><i class="bi bi-facebook" id="fb"></i></a><a href="https://www.snapchat.com/add/l.cugnet?share_id=yNruLtL4VQ0&locale=fr-FR" target="_blank"><i class="bi bi-snapchat" id="snap"></i></a></br>Copyright &copy '+theDate.getFullYear()+' CUGNET Lucas. Tous droits réservés.</p>';
+    document.getElementById("footer").innerHTML = '<p><a href="https://www.instagram.com/lucas_cugnet/" target="_blank"><i class="bi bi-instagram" id="insta"></i></a><a href="https://www.facebook.com/people/Lucas-Cugnet/100008483671392/" target="_blank"><i class="bi bi-facebook" id="fb"></i></a><a href="https://www.snapchat.com/add/l.cugnet?share_id=yNruLtL4VQ0&locale=fr-FR" target="_blank"><i class="bi bi-snapchat" id="snap"></i></a></br><span onclick="loadLegal()">Copyright &copy '+theDate.getFullYear()+' CUGNET Lucas. Tous droits réservés.</span></p>';
 }
+
+
+//form-----------------------------------------------------------------------------------------------------
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-green-500");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-red-500");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Quelque chose s'est mal passé!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
+    });
+});
